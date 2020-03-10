205353e34be782c05fb2e4e93bc4d3f73f320afe
JinWoo-Choi-cs <wlsdngjwjq@gmail.com>
2020-03-10 11:10:08 +0900

    Model Testing~

---
+++ RunTest/CreatePost.py
@@ -0,0 +1,52 @@
+encoding : utf-8
+from blog.models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.shortcuts import render
+
+
+# region-s Testing Done
+#* 만들기
+# Post.objects.create(author=User.objects.get(username='admin'), title='Created Testing', text="???")
+
+#print ("------------- before -------------")
+#print (Post.objects.all()) # user = admin
+
+#print ("------------- after -------------")
+# me = User.objects.get(username='admin')
+# Post.objects.filter(author=me, title='Post').delete()
+# print (Post.objects.filter(title__contains='Post'))
+# Post.objects.filter(author=me, title__contains='Post').delete()
+# Post.objects.create(author=me, title='Post made by run file', text='Testing')
+
+#for a in Post.objects.all() :
+#    print ("----------------" + a.title)
+
+#* 5~10  번쨰까지만
+# Post.objects.all()[5:10]     
+#* ~ 5  번쨰까지만 
+# Post.objects.all()[:5]      
+#* ~10  번쨰까지 중에 2개당 1개씩  
+# Post.objects.all()[:10:2]     
+
+#* 하나 발행
+#Post.objects.get(title="Created Testing").publish()
+
+#* 전부 발행
+#for a in Post.objects.all():
+#    a.publish()
+
+#* 현재보다 빨리 발행된것 찾기
+#Post.objects.filter(published_date__lte=timezone.now())
+#* 오름차순 정리
+#Post.objects.order_by('created_date')
+#* 내림차순 정리
+#Post.objects.order_by('-created_date')
+#* 더블 쿼리 (쿼리 연결 : chaining)
+#Post.objects.filter(published_date__lte=timezone.now()).order_by('created_date')
+#Post.objects.filter(published_date__lte=timezone.now()).order_by('-created_date')
+
+# region-e Testing Done
+
+print()
+
--- blog/models.py
+++ blog/models.py
@@ -2,8 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
-class Post(models.Model):
+class Post(models.Model):   #models.Model은 DB에 저장되어야 하는 객체임을 알려준다.
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -18,3 +17,61 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+'''
+
+class PostWithReply(models.Model):
+    index = models.index()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    reply = models.Reply()
+
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):  # ! 포매팅해서 반환
+        _str = StringIO()   
+        _str.write("Index = ")
+        _str.write(str(self.index))
+        _str.write(" / title = ")
+        _str.write(self.title)
+        
+        return _str.getvalue()
+
+class Reply(models.Model):
+    post_index = models.ForeignKey(models.PostWithReply.index, on_delete=models.CASCADE)  # 답변을 단 Post의 인덱스 CASCADE부분 모르겠네
+    index = models.index()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        _str = StringIO()   
+        _str.write("Post's Index = ")
+        _str.write(str(self.post_index))
+        _str.write("/ Reply's Index = ")
+        _str.write(str(self.index))
+        _str.write(" / title = ")
+        _str.write(self.title)
+        
+        return _str.getvalue()
+
+'''
