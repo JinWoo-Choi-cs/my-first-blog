e0584aa4dcbac29bc5784a9f28e60f5bfc0fe452
JinWoo-Choi-cs <wlsdngjwjq@gmail.com>
2020-03-10 12:20:18 +0900

    fetch: Template Language Insert

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.utils import timezone
 from .models import Post
-# Create your views here.
+# . (dot)은 현재 디렉토리 혹은 어플을 의미한다.
 
+
+#* Create your views here.
+
+#** 템플릿 파일에서 아래 return 값을 활용한다.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
+    # return : {}내부에 keys(문자열) : values(값) 으로 매개변수 넘겨줌
+    return render(request, 'blog/post_list.html', {'Posts' : posts})
+
+
+#
+#*
+#**
+#!
+#?
+#region-s
+#region-e
+#todo
+# //param
 
